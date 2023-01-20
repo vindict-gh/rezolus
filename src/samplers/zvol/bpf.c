4ca790c3b045caaaae4f4d0eed2199d66312791c
Valentin BRICE <dev@vbrice.fr>
2023-01-20 17:54:47 +0100

    [WIP] Add zvol name to results

--- src/samplers/zvol/bpf.c
+++ src/samplers/zvol/bpf.c
@@ -6,18 +6,43 @@
 // Based on: ../xfs/bpf.c
 
 #include <uapi/linux/ptrace.h>
-#include <sys/zvol_impl.h>
 
 #define OP_CODE_READ 0
 #define OP_CODE_WRITE 1
 
 #define ZFS_MAXNAMELEN 256
 
+// XXX: Incomplete declaration of ZFS's internal zv_request_stack
+// We only care here about the pointer to zvol_state_t to access to the zvol name
 typedef struct zv_request_stack {
 	zvol_state_t	*zv;
-	struct bio	*bio;
-	struct request *rq;
+//	struct bio	*bio;
+//	struct request *rq;
 } zv_request_t;
+
+// XXX: Incomplete declaration of ZFS's internal zvol_state
+// We only care here about the pointer to zv_name to access to the zvol name
+typedef struct zvol_state {
+    char			zv_name[ZFS_MAXNAMELEN];
+//	uint64_t		zv_volsize;
+//	uint64_t		zv_volblocksize;
+//	objset_t		*zv_objset;
+//	uint32_t		zv_flags;
+//	uint32_t		zv_open_count;
+//	uint32_t		zv_changed;
+//	uint32_t		zv_volmode;
+//	zilog_t			*zv_zilog;
+//	zfs_rangelock_t		zv_rangelock;
+//	dnode_t			*zv_dn;
+//	dataset_kstats_t	zv_kstat;
+//	list_node_t		zv_next;
+//	uint64_t		zv_hash;
+//	struct hlist_node	zv_hlink;
+//	kmutex_t		zv_state_lock;
+//	atomic_t		zv_suspend_ref;
+//	krwlock_t		zv_suspend_lock;
+//	struct zvol_state_os	*zv_zso;
+} zvol_state_t;
 
 typedef struct dist_key {
     char zvol_name[ZFS_MAXNAMELEN];
@@ -45,7 +70,9 @@ int trace_entry(struct pt_regs *ctx, zv_request_t *zvr)
     u64 ts = bpf_ktime_get_ns();
     struct hash_value value = {.ts = ts};
 
-    bpf_probe_read_kernel(&value.zvol_name, sizeof(value.zvol_name), zvr->zv.zvol_name);
+    // arguably we could also write zvr->zv.zv_name just as zvr->zv
+    // per C standard definitions
+    bpf_probe_read_kernel(&value.zvol_name, sizeof(value.zvol_name), zvr->zv->zv_name);
 
     start.update(&tid, &value);
     return 0;
@@ -56,6 +83,7 @@ static int trace_return(struct pt_regs *ctx, const int op)
     u64 pid_tgid = bpf_get_current_pid_tgid();
     u32 pid = pid_tgid >> 32;
     u32 tid = (u32)pid_tgid;
+    u64 cur_ts = bpf_ktime_get_ns()
 
     // lookup start time
     hash_value_t *start_value = start.lookup(&tid);
@@ -66,7 +94,7 @@ static int trace_return(struct pt_regs *ctx, const int op)
     }
 
     // calculate latency in microseconds
-    u64 delta = (bpf_ktime_get_ns() - start_value->ts) / 1000;
+    u64 delta = (cur_ts - start_value->ts) / 1000;
 
     struct dist_key key = {};
     __builtin_memcpy(&key.zvol_name, start_value->zvol_name, sizeof(key.zvol_name));
